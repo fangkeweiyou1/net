6c45821ea56bd42833514e060292b04f782c5099
zhangyuncai <zhangyc@asia5b.com>
2020-05-11 09:56:22 +0800

    新增代码

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var util = require('util');
+var url = require('url');
+var request = require('request');
 //解决跨域
 var cors = require('cors');
 
@@ -18,36 +22,149 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-//
-//
-//
-app.get('/aaa', function (req, res) {
-    res.send('Hello World');
-})
+//todo
+//todo
+//todo
 
-//设置跨域访问
-app.all('*', function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "https://www.runoob.com");
-    // res.header("Access-Control-Allow-Origin", "https://fcm.googleapis.com");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type,Access-Token");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1');
-    res.header("Content-Type", "application/json;charset=utf-8");
-    console.log('跨域访问')
-    res.end(JSON.stringify({
-        data: 'hello world!'
-    }))
+/**
+ * todo 读取文件目录
+ */
+app.get('/readDir', function (req, res) {
+    var params = req.query
+    console.log('打印参数:' + params.type)
+    var dirPath = "files/";
+    switch (params.type) {
+        case 'wms': {
+            dirPath = dirPath + "wms"
+        }
+            break;
+        case 'newasia5b_cn': {
+            dirPath = dirPath + "newasia5b/cn"
+        }
+            break;
+        case 'newasia5b_com': {
+            dirPath = dirPath + "newasia5b/com"
+        }
+            break;
+        case 'jy_cn': {
+            dirPath = dirPath + "jy/com"
+        }
+            break;
+        case 'jy_com': {
+            dirPath = dirPath + "jy/com"
+        }
+            break;
+        case 'shop_cn': {
+            dirPath = dirPath + "shop/cn"
+        }
+            break;
+        case 'shop_com': {
+            dirPath = dirPath + "shop/com"
+        }
+            break;
+        case 'pay_cn': {
+            dirPath = dirPath + "pay/cn"
+        }
+            break;
+        case 'pay_com': {
+            dirPath = dirPath + "pay/com"
+        }
+            break;
+        case 'live_cn': {
+            dirPath = dirPath + "live/cn"
+        }
+            break;
+        case 'live_com': {
+            dirPath = dirPath + "live/com"
+        }
+            break;
+        case 'other':
+            dirPath = dirPath + "other"
+            break;
+
+        default:
+            break;
+    }
+
+
+    var queryPath = path.join(__dirname, 'public', dirPath)
+    fs.readdir(queryPath, function (err, files) {
+        if (err) {
+            res.send(err);
+            return console.error(err);
+        }
+        console.log("文件数量:" + files.length);
+        var resultFiles = [];
+        files.forEach(function (file) {
+            var fileBean = {
+                filename: file,
+                filepath: "http://" + req.headers.host + "/" + dirPath + '/' + file
+            }
+            resultFiles.push(fileBean);
+            console.log("文件名称:" + file);
+        });
+        res.send(resultFiles)
+    });
 });
 
-app.listen(8000)
 
-console.log('服务器启动成功')
+/**
+ *  todo 上传文件
+ */
+app.post('/uploadFile', function (req, res) {
+    var inputFile = req.files[0];//From the name
+    console.log(inputFile);// 上传的文件信息
+    /**
+     * todo createFormData  (image)  中第一个参数文件名一定要与后台完全一致,否则报500
+     */
+    // var file = req.files.image;//From the name
+    console.log('文件类型：%s', inputFile.type);
+    console.log('原始文件名：%s', inputFile.name);
+    console.log('文件大小：%s', inputFile.size);
+    console.log('文件保存路径：%s', inputFile.path);
+    // res.send('上传文件成功')
+    var des_file = __dirname + "/public/files/" + inputFile.originalname;//指定保存目录
+    fs.readFile(inputFile.path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                response = {
+                    message: 'File uploaded successfully',
+                    filename: inputFile.originalname
+                };
+            }
+            console.log(response);
+            res.end(JSON.stringify(response));
+        });
+    });
+});
+
+/**
+ * todo 查看某个公众号历史数据,学习访问其他的API
+ */
+app.get('/other', function (req, res) {
+    request('https://wanandroid.com/wxarticle/list/408/1/json', function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+
+            body = JSON.parse(body);
+            res.send(body);
+
+        } else {
+            res.send('error');
+        }
+    })
+});
+
+app.listen(8000);
+
+console.log('服务器启动成功');
 
 //
 //
@@ -58,19 +175,19 @@ app.use('/users', usersRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
